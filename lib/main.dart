60711ce6ed3a86d56d763853670407ae14c6a6ba
ChrisRgr <christopher_roger@orange.fr>
2021-12-03 16:47:34 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:todo_application/models/todo_model.dart';
-import 'package:todo_application/models/todo.dart';
+import 'package:project1/todo_model/todo.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: MyApp(items: [Todo(id: 1,title: "Todo 1",description: "Une description"),
-      Todo(id: 2,title: "Todo 2",description: "Une deuxième description"),
-      Todo(id: 1,title: "Todo 3",description: "Une troisième description")]
-      ,),
+  runApp(
+      MaterialApp(
+          home: MyApp(
+            items: [
+              Todo(id: 1, title: "Todo 1", description: "Une description"),
+              Todo(id: 2, title: "Todo 2", description: "Une deuxième description"),
+              Todo(id: 1, title: "Todo 3", description: "Une troisième description")
+            ],
+  ),
   ),
   );
 }
@@ -19,10 +22,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "test",
+      title: "Mon app Todo",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("test"),
+          title: const Text("Ma liste"),
         ),
         body: ListView.builder(
           itemCount: items.length,
@@ -49,67 +52,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  void _AddTodo() {
-    setState(() {
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _AddTodo,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class AddPage extends StatelessWidget{
+class AddPage extends StatelessWidget {
   const AddPage({Key? key}) : super(key: key);
 
   @override
@@ -119,11 +62,106 @@ class AddPage extends StatelessWidget{
         title: const Text("Formulaire d'ajout"),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+        child: Column(
+          children: [
+            //Form d'ajout
+            const AddTaskForm(),
+            // Back button
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Revenir en arrière'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Create a Form widget.
+class AddTaskForm extends StatefulWidget {
+  const AddTaskForm({Key? key}) : super(key: key);
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<AddTaskForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Merci d'entrer une valeur valide";
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding:
+            const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a confirmation page.
+                  Navigator.push(
+                    context,
+                    // Il est possible d'ajouter des arguments qui seront passés au constructeur
+                    MaterialPageRoute(builder: (context) => ConfirmPage()),
+                  );
+                }
+              },
+              child: const Text('Enregistrer'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class ConfirmPage extends StatelessWidget {
+  const ConfirmPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Confirmation d'ajout de la tâche"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Bravo ! C'est ajouté ;)"),
+            // Back button
+            ElevatedButton(
+              onPressed: () {
+                //Retour home page
+                Navigator.of(context).popUntil((route) => route.isFirst);
+              },
+              child: const Text("Revenir à la page d'accueil"),
+            ),
+          ],
         ),
       ),
     );
